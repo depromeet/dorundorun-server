4b92123136d09177bea16a9b74b4efada12009a9
heogeonho <geonh1016@gmail.com>
2025-08-22 19:53:45 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/test/java/com/example/team6server/Team6ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team6server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team6ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
