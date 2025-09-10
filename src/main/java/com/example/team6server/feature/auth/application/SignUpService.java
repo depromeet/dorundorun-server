d769fc05d8a2ecb8233e2f319e1ab3a285c5ad17
Hoya324 <hoyana1225@gmail.com>
2025-09-11 00:46:09 +0900

    feat: auth password 해시(BCrypt 등) 적용

--- src/main/java/com/example/team6server/feature/auth/application/SignUpService.java
+++ src/main/java/com/example/team6server/feature/auth/application/SignUpService.java
@@ -5,6 +5,7 @@ import com.example.team6server.feature.auth.dto.response.SignUpResponse;
 import com.example.team6server.feature.user.dao.UserRepository;
 import com.example.team6server.feature.user.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,8 @@ public class SignUpService {
 
 	private final ValidateSignUpPolicyService validateSignUpPolicyService;
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
 
 	@Transactional
 	public SignUpResponse signUp(SignUpRequest request) {
@@ -22,7 +25,7 @@ public class SignUpService {
 		User user = User.builder()
 				.name(request.name())
 				.email(request.email())
-				.password(request.password()) // TODO: 암호화 추가
+				.password(passwordEncoder.encode(request.password()))
 				.build();
 
 		User savedUser = userRepository.save(user);
--- src/main/java/com/example/team6server/global/config/security/SecurityConfig.java
+++ src/main/java/com/example/team6server/global/config/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -19,52 +21,57 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    @Order(1)
-    public SecurityFilterChain swaggerFilterChain(HttpSecurity http) throws Exception {
-        defaultFilterChain(http);
+	@Bean
+	@Order(1)
+	public SecurityFilterChain swaggerFilterChain(HttpSecurity http) throws Exception {
+		defaultFilterChain(http);
 
-        http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**");
-        http.httpBasic(withDefaults());
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+		http.securityMatcher("/swagger-ui/**", "/v3/api-docs/**");
+		http.httpBasic(withDefaults());
+		http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        defaultFilterChain(http);
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		defaultFilterChain(http);
 
-        http.exceptionHandling(exception -> exception
-                        .authenticationEntryPoint((request, response, authException) -> {
-                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-                        })
-                ) // TODO 예외 응답 형식 통일
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/api/health").permitAll()
-                        .requestMatchers("/api/**").permitAll()
-                        .anyRequest().authenticated());
+		http.exceptionHandling(exception -> exception
+						.authenticationEntryPoint((request, response, authException) -> {
+							response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+						})
+				) // TODO 예외 응답 형식 통일
+				.authorizeHttpRequests(authorize -> authorize
+						.requestMatchers("/public/**").permitAll()
+						.requestMatchers("/api/health").permitAll()
+						.requestMatchers("/api/**").permitAll()
+						.anyRequest().authenticated());
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    private void defaultFilterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(AbstractHttpConfigurer::disable).formLogin(AbstractHttpConfigurer::disable).cors(withDefaults()).csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-    }
+	private void defaultFilterChain(HttpSecurity http) throws Exception {
+		http.httpBasic(AbstractHttpConfigurer::disable).formLogin(AbstractHttpConfigurer::disable).cors(withDefaults()).csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	}
 
-    // TODO: 임시 cors 설정
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
+	// TODO: 임시 cors 설정
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOriginPattern("*");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
 
