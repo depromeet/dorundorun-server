5a639cd04a6a15b32d1f21cd5361bab9732c082f
Hoya324 <hoyana1225@gmail.com>
2025-09-11 00:58:10 +0900

    feat: validation 추가

--- src/main/java/com/example/team6server/feature/auth/api/AuthController.java
+++ src/main/java/com/example/team6server/feature/auth/api/AuthController.java
@@ -3,10 +3,12 @@ package com.example.team6server.feature.auth.api;
 import com.example.team6server.feature.auth.application.SignUpService;
 import com.example.team6server.feature.auth.dto.request.SignUpRequest;
 import com.example.team6server.feature.auth.dto.response.SignUpResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +18,7 @@ public class AuthController implements AuthApi {
 	private final SignUpService signUpService;
 
 	@PostMapping("/api/auth/signup")
-	public ResponseEntity<SignUpResponse> signUp(SignUpRequest request) {
+	public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest request) {
 		SignUpResponse response = signUpService.signUp(request);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
