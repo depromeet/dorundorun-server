e0fa7750743bbf005a37fc8655f3bf594e69fc02
Hoya324 <hoyana1225@gmail.com>
2025-09-12 19:55:06 +0900

    feat: slack webhook setting

---
+++ src/main/java/com/example/team6server/infra/slack/SlackNotifierImpl.java
@@ -0,0 +1,33 @@
+package com.example.team6server.infra.slack;
+
+import com.example.team6server.global.config.slack.SlackProperties;
+import com.slack.api.Slack;
+import com.slack.api.webhook.Payload;
+import com.slack.api.webhook.WebhookResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SlackNotifierImpl implements SlackNotifier {
+
+	private final SlackProperties props;
+
+	private final Slack slackClient = Slack.getInstance();
+
+	@Override
+	public WebhookResponse send(String paramText) {
+		Payload payload = Payload.builder().text(paramText).build();
+		WebhookResponse response;
+		try {
+			response = slackClient.send(props.webhookUrl(), payload);
+			return response;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
