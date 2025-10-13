8f8b7e156b3d9ce16060b8d62344543f11a2c9f5
heogeonho <geonh1016@gmail.com>
2025-10-13 22:44:38 +0900

    refactor: 불필요한 goal 관련 엔티티 및 RunningLevel enum 제거

--- src/main/java/com/sixpack/dorundorun/feature/goal/dao/GoalPlanJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sixpack.dorundorun.feature.goal.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sixpack.dorundorun.feature.goal.domain.GoalPlan;
-
-public interface GoalPlanJpaRepository extends JpaRepository<GoalPlan, Long> {
-}
