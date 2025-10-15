c56e05a261b587cd28c525b5479af834dcfc6278
Hoya324 <hoyana1225@gmail.com>
2025-10-15 14:46:58 +0900

    feat: run 러닝 기록 조회 기능 추가

--- src/main/java/com/sixpack/dorundorun/feature/run/GoalRepeatType.java
+++
@@ -1,5 +0,0 @@
-package com.sixpack.dorundorun.feature.run;
-
-public enum GoalRepeatType {
-	WEEK
-}
---
+++ src/main/java/com/sixpack/dorundorun/feature/run/application/FindAllRunSessionsService.java
@@ -0,0 +1,47 @@
+package com.sixpack.dorundorun.feature.run.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sixpack.dorundorun.feature.run.dao.RunSessionJpaRepository;
+import com.sixpack.dorundorun.feature.run.dao.projection.RunSessionWithFeedProjection;
+import com.sixpack.dorundorun.feature.run.dto.request.RunSessionListRequest;
+import com.sixpack.dorundorun.feature.run.dto.response.RunSessionListResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FindAllRunSessionsService {
+
+	private final RunSessionJpaRepository runSessionJpaRepository;
+
+	@Transactional(readOnly = true)
+	public List<RunSessionListResponse> find(Long userId, RunSessionListRequest request) {
+		List<RunSessionWithFeedProjection> results = runSessionJpaRepository.findAllBySefiedStatusAndStartDateTime(
+			userId,
+			request.isSefied(),
+			request.startDateTime()
+		);
+
+		return results.stream()
+			.map(this::mapToResponse)
+			.toList();
+	}
+
+	private RunSessionListResponse mapToResponse(RunSessionWithFeedProjection projection) {
+		return new RunSessionListResponse(
+			projection.getId(),
+			projection.getCreatedAt(),
+			projection.getUpdatedAt(),
+			projection.getFinishedAt(),
+			projection.getDistanceTotal(),
+			projection.getDurationTotal(),
+			projection.getPaceAvg(),
+			projection.getCadenceAvg(),
+			projection.getIsSefied()
+		);
+	}
+}
