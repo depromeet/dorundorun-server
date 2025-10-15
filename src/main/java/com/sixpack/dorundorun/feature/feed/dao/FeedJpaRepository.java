1af482699e64f35b145a6fa67f5cdadf73f63628
Hoya324 <hoyana1225@gmail.com>
2025-10-15 20:24:28 +0900

    feat: feed 피드 조회 쿼리 추가

--- src/main/java/com/sixpack/dorundorun/feature/feed/dao/FeedJpaRepository.java
+++ src/main/java/com/sixpack/dorundorun/feature/feed/dao/FeedJpaRepository.java
@@ -1,8 +1,12 @@
 package com.sixpack.dorundorun.feature.feed.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sixpack.dorundorun.feature.feed.domain.Feed;
 
 public interface FeedJpaRepository extends JpaRepository<Feed, Long> {
+	
+	Optional<Feed> findByRunSessionIdAndDeletedAtIsNull(Long runSessionId);
 }
