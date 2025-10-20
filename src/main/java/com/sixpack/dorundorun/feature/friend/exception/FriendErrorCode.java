b809c51196abd5b4689f2b6ad8eac108bc945b39
1026hz <1026hzz@gmail.com>
2025-10-21 03:20:40 +0900

    fix: 같은 예외처리로 묶기

--- src/main/java/com/sixpack/dorundorun/feature/friend/exception/FriendErrorCode.java
+++ src/main/java/com/sixpack/dorundorun/feature/friend/exception/FriendErrorCode.java
@@ -12,9 +12,10 @@ import lombok.Getter;
 public enum FriendErrorCode implements ErrorCode {
 
 	NOT_FOUND_USER_BY_CODE(HttpStatus.NOT_FOUND, "해당 코드의 유저를 찾을 수 없습니다. [code: %s]"),
+	NOT_FOUND_FRIEND(HttpStatus.NOT_FOUND, "해당 친구를 찾을 수 없습니다. [friendId: %s]"),
+	
 	ALREADY_FRIEND(HttpStatus.BAD_REQUEST, "이미 친구로 등록된 유저입니다. [userId: %s]"),
-	CANNOT_ADD_SELF_AS_FRIEND(HttpStatus.BAD_REQUEST, "자기 자신을 친구로 추가할 수 없습니다."),
-	NOT_FOUND_FRIEND(HttpStatus.NOT_FOUND, "해당 친구를 찾을 수 없습니다. [friendId: %s]");
+	CANNOT_ADD_SELF_AS_FRIEND(HttpStatus.BAD_REQUEST, "자기 자신을 친구로 추가할 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String message;
