5a7d69e2ef2942c522faf3449f9948889ca928d3
1026hz <1026hzz@gmail.com>
2025-10-29 18:44:17 +0900

    feat: friends running status 조회 api에 latestCheeredAt 항목 추가

--- src/main/java/com/sixpack/dorundorun/feature/notification/dao/NotificationJpaRepository.java
+++ src/main/java/com/sixpack/dorundorun/feature/notification/dao/NotificationJpaRepository.java
@@ -1,5 +1,8 @@
 package com.sixpack.dorundorun.feature.notification.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
