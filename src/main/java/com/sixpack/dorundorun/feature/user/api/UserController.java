4e29dadfea3b879a1c6ca03241c6b2e77b0f5008
heogeonho <geonh1016@gmail.com>
2025-11-04 03:49:08 +0900

    feat: 사용자 프로필 조회 및 수정 API 명세 추가

--- src/main/java/com/sixpack/dorundorun/feature/user/api/UserController.java
+++ src/main/java/com/sixpack/dorundorun/feature/user/api/UserController.java
@@ -1,10 +1,14 @@
 package com.sixpack.dorundorun.feature.user.api;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sixpack.dorundorun.feature.user.domain.User;
-import com.sixpack.dorundorun.feature.user.dto.response.UserResponse;
+import com.sixpack.dorundorun.feature.user.dto.response.MeProfileResponse;
 import com.sixpack.dorundorun.global.aop.annotation.CurrentUser;
 import com.sixpack.dorundorun.global.response.DorunResponse;
 
@@ -15,9 +19,19 @@ import lombok.RequiredArgsConstructor;
 public class UserController implements UserApi {
 
 	@Override
-	@GetMapping("/api/users/me")
-	public DorunResponse<UserResponse> getMyInfo(@CurrentUser User currentUser) {
-		UserResponse response = UserResponse.of(currentUser);
+	@GetMapping("/api/users/me/profile")
+	public DorunResponse<MeProfileResponse> getMeProfile(@CurrentUser User currentUser) {
+		MeProfileResponse response = MeProfileResponse.of(currentUser);
 		return DorunResponse.success(response);
+	}
+
+	@Override
+	@PatchMapping(value = "/api/users/me", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public DorunResponse<Void> updateMeProfile(
+		@CurrentUser User currentUser,
+		@RequestPart(value = "data") String dataJson,
+		@RequestPart(value = "profileImage", required = false) MultipartFile profileImage
+	) {
+		return DorunResponse.success("프로필 수정에 성공하였습니다");
 	}
 }
