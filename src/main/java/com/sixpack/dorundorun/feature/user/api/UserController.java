7068f319c96b5ecb421eeedc0c5e56942ba4e62e
1026hz <1026hzz@gmail.com>
2025-11-05 12:39:25 +0900

    feat: user - fcm device token 수정 api

--- src/main/java/com/sixpack/dorundorun/feature/user/api/UserController.java
+++ src/main/java/com/sixpack/dorundorun/feature/user/api/UserController.java
@@ -3,13 +3,16 @@ package com.sixpack.dorundorun.feature.user.api;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.sixpack.dorundorun.feature.user.application.FindMyProfileService;
+import com.sixpack.dorundorun.feature.user.application.UpdateDeviceTokenService;
 import com.sixpack.dorundorun.feature.user.application.UpdateMyProfileService;
 import com.sixpack.dorundorun.feature.user.domain.User;
+import com.sixpack.dorundorun.feature.user.dto.request.DeviceTokenUpdateRequest;
 import com.sixpack.dorundorun.feature.user.dto.response.MyProfileResponse;
 import com.sixpack.dorundorun.feature.user.dto.response.NewProfileResponse;
 import com.sixpack.dorundorun.global.aop.annotation.CurrentUser;
@@ -23,6 +26,7 @@ public class UserController implements UserApi {
 
 	private final FindMyProfileService findMyProfileService;
 	private final UpdateMyProfileService updateMyProfileService;
+	private final UpdateDeviceTokenService updateDeviceTokenService;
 
 	@Override
 	@GetMapping("/api/users/me/profile")
@@ -40,5 +44,15 @@ public class UserController implements UserApi {
 	) {
 		NewProfileResponse response = updateMyProfileService.updateProfile(currentUser, dataJson, profileImage);
 		return DorunResponse.success(response);
+	}
+
+	@Override
+	@PatchMapping("/api/users/device-token")
+	public DorunResponse<Void> updateDeviceToken(
+		@CurrentUser User currentUser,
+		@RequestBody DeviceTokenUpdateRequest request
+	) {
+		updateDeviceTokenService.updateDeviceToken(currentUser, request);
+		return DorunResponse.success("fdm device token 업데이트가 성공적으로 진행됐습니다.", null);
 	}
 }
