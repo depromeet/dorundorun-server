7be769e3ad0723bf99e9bf13f85bf737712979f9
heogeonho <geonh1016@gmail.com>
2025-11-06 15:31:15 +0900

    fix: 디폴트 프로필 이미지 저장 및 조회 관련 이슈 해결

--- src/main/java/com/sixpack/dorundorun/feature/auth/application/UploadProfileImageService.java
+++ src/main/java/com/sixpack/dorundorun/feature/auth/application/UploadProfileImageService.java
@@ -1,6 +1,5 @@
 package com.sixpack.dorundorun.feature.auth.application;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,12 +13,9 @@ public class UploadProfileImageService {
 
 	private final S3Service s3Service;
 
-	@Value("${app.default-profile-image-url}")
-	private String defaultProfileImageUrl;
-
 	public String upload(MultipartFile profileImage) {
 		if (profileImage == null || profileImage.isEmpty()) {
-			return defaultProfileImageUrl;
+			return null;
 		}
 
 		return s3Service.uploadImage(profileImage);
