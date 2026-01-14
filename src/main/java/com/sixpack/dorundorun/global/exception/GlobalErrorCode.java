5d5626f3999c8769ef8142d771fea853d87e9b9b
1026hz <1026hzz@gmail.com>
2026-01-14 14:10:02 +0900

    fix: 만료된 JWT 에러핸들링

--- src/main/java/com/sixpack/dorundorun/global/exception/GlobalErrorCode.java
+++ src/main/java/com/sixpack/dorundorun/global/exception/GlobalErrorCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum GlobalErrorCode implements ErrorCode {
 
 	INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력 값입니다"),
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다"),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다");
 
 	private final HttpStatus status;
